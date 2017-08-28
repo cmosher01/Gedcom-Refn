02559beeb30d6af6129f5cfb77461495a7fb5b51
Christopher A. Mosher <cmosher01@gmail.com>
2017-08-27 20:33:05 -0400

    refactor

--- src/main/java/nu/mine/mosher/gedcom/GedcomRefn.java
+++ src/main/java/nu/mine/mosher/gedcom/GedcomRefn.java
@@ -4,17 +4,15 @@ import nu.mine.mosher.collection.TreeNode;
 import nu.mine.mosher.gedcom.exception.InvalidLevel;
 import nu.mine.mosher.mopper.ArgParser;
 
-import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.util.*;
+import java.io.IOException;
+import java.util.UUID;
 
 import static nu.mine.mosher.logging.Jul.log;
 
 /**
  * For each top-level record (INDI, FAM, SOUR, etc.) that does not have a REFN,
  * add one with a UUID for a value.
- *
+ * <p>
  * Created by user on 12/12/16.
  */
 public class GedcomRefn implements Gedcom.Processor {
@@ -38,45 +36,16 @@ public class GedcomRefn implements Gedcom.Processor {
 
     @Override
     public boolean process(final GedcomTree tree) {
-//        addNodes(createNewRefns(tree.getRoot(), new ArrayList<>(4096)));
-        createNewRefns(tree.getRoot());
-        return true;
-    }
-
-
-
-    private static class ChildToBeAdded {
-        TreeNode<GedcomLine> parent;
-        TreeNode<GedcomLine> child;
-        ChildToBeAdded(TreeNode<GedcomLine> parent, TreeNode<GedcomLine> child) {
-            this.parent = parent;
-            this.child = child;
-        }
-    }
-
-    private void /*List<ChildToBeAdded>*/ createNewRefns(final TreeNode<GedcomLine> root/*, final List<ChildToBeAdded> refns*/) {
-        root.forEach(top -> {
+        tree.getRoot().forEach(top -> {
             final GedcomLine gedcomLine = top.getObject();
             if (gedcomLine != null && gedcomLine.hasID()) {
                 if (!hasRefn(top)) {
-//                    refns.add(new ChildToBeAdded(top, new TreeNode<>(gedcomLine.createChild(GedcomTag.REFN, UUID.randomUUID().toString()))));
                     top.addChild(new TreeNode<>(gedcomLine.createChild(GedcomTag.REFN, UUID.randomUUID().toString())));
                 }
             }
         });
-//        return refns;
+        return true;
     }
-
-
-
-//    private static void addNodes(final List<ChildToBeAdded> adds) {
-//        adds.forEach(add -> {
-//            add.parent.addChild(add.child);
-//        });
-//    }
-
-
-
 
 
 
